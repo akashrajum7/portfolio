2e2fd3e292858bb70505655aaa675902be278898
Akash Raju M <akashrajum7@gmail.com>
2020-04-11 23:52:46 +0530

    add dp and hello message

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to my portfolio" />
+    <HomePage />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HomePage from "./components/HomePage.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HomePage
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Rubik&display=swap");
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+  font-family: "Rubik", sans-serif;
+}
+html {
+  height: 100vh;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <div class="dp-cont">
+      <img src="@/assets/profile-photo.jpg" alt="Profile photo" height="100" width="100" />
+      <h1 class="name">Akash Raju M</h1>
+      <p class="hello">Hello there 👋</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.dp-cont {
+  display: grid;
+  text-align: center;
+}
+.dp-cont img {
+  margin: auto;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-top: 2rem;
+}
+.name {
+  margin-top: 0.5rem;
+}
+.hello {
+  margin-top: 2rem;
+}
+</style>

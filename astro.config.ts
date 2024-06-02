2c53d8b29456f46d151c5eab366b13abf7598ad8
akashrajum7 <akashrajum7@gmail.com>
2024-06-02 20:57:36 +0530

    feat: Make output as hybrid

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  output: "hybrid",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
